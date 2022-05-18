502335a3f0aef51a2db327afe0c7f2c55b78ab21
nabil-y <nabil.yassine.dev@gmail.com>
2022-05-19 00:07:24 +0200

    refactor code and reorganize folders

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import Header from "./components/Layout/Header";
 import Footer from "./components/Layout/Footer";
-import DropdownLarge from "./components/UI/Stateful/DropdownLarge";
+import Dropdown from "./components/Dropdown/Dropdown";
 import { Routes, Route } from "react-router-dom";
+import Loader from "./components/Layout/Loader";
 
 const App = () => {
   return (
     <>
       <Header />
+      {/* <Loader /> */}
       <Routes>
         <Route
           path="/about"
-          element={<DropdownLarge title="testTitle" info="testInfo" />}
+          element={<Dropdown title="testTitle" info="testInfo" />}
         />
         <Route path="/" exact element={<Footer />} />
       </Routes>
---
+++ src/components/Dropdown/Dropdown.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import Chevron from "../UI/SVG/Chevron";
+import styles from "./Dropdown.module.css";
+
+const Dropdown = (props) => {
+  return (
+    <details className={styles.details}>
+      <summary>
+        {props.title} <Chevron />
+      </summary>
+      <p>{props.info}</p>
+    </details>
+  );
+};
+
+export default Dropdown;
